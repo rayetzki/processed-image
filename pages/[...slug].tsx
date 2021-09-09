ffd91558cbc80f3c57d4c2a4174a7cf213ed24f2
rayetzki <rayetzkiillya@gmail.com>
2021-09-09 16:31:12 +0300

    [Revert]: Get images in real-time

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -45,21 +45,7 @@ const BasicPage = ({
 
 export default BasicPage;
 
-export async function getStaticPaths() {
-  return {
-    paths: [
-      { params: { slug: ['animals'] } },
-      { params: { slug: ['people'] } },
-      { params: { slug: ['minimalism'] } },
-      { params: { slug: ['plants'] } },
-      { params: { slug: ['blacknwhite'] } },
-      { params: { slug: ['landscapes'] } },
-    ],
-    fallback: true,
-  };
-}
-
-export async function getStaticProps(context: GetStaticPropsContext) {
+export async function getServerSideProps(context: GetStaticPropsContext) {
 	const folder = context.params?.slug && context.params.slug[0];
 	if (!folder) return;
 	try {
