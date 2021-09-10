7c4db41919714e17c5f15fccd5006274d7400d1c
rayetzki <rayetzkiillya@gmail.com>
2021-09-10 12:22:39 +0300

    [Client]: Responsiveness fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,10 +8,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ru">
-        <Head />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet" />
+        <Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+					<link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet" />
+					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				</Head>
         <body>
           <Main />
           <NextScript />
--- pages/layout/Navbar.tsx
+++ pages/layout/Navbar.tsx
@@ -1,7 +1,6 @@
-import { Fragment, useEffect, useState } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useMediaQuery } from 'react-responsive'; 
 import styles from "./Navbar.module.css";
 import cx from "classnames";
 
@@ -48,71 +47,79 @@ const MobileRoutes = [
 	}))
 ];
 
-
-export default function Navbar() {
+function DesktopNav() {
   const { asPath: currentRoute } = useRouter();
-	const isMobile = useMediaQuery({ maxWidth: 768 });
-	const [isMobileMenuExpanded, setMobileMenuExpanded] = useState(false);
 
-  return (
-    <nav aria-label="Основная навигация" className={styles.Nav}>
-      {!isMobile && Routes.map((route) => (
-        <Fragment key={route.caption}>
-          <div role="menuitem" className={cx(styles.LinkBlock, {
-            [styles.Active]: route.link === currentRoute
-          })}>
-            <Link href={route.link || '/'}>
-              <a className={styles.Link} aria-haspopup={!!route.subRoutes}>
-                {route.caption}
-              </a>
-            </Link>
-            {route.subRoutes && (
-              <menu className={styles.SubMenu}>
-                <ul className={styles.SubMenuList}>
-                  {route.subRoutes.map((subRoute) => (
-                    <li key={subRoute.link} className={cx(styles.LinkBlock, {
-                      [styles.Active]: subRoute.link === currentRoute
-                    })}>
-                      <Link href={subRoute.link}>
-                        <a className={styles.SubMenuLink}>{subRoute.caption}</a>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </menu>
-            )}
-          </div>
-        </Fragment>
-      ))}
-			{isMobile && (
-				<Fragment>
-					<h2 className={styles.NavMobileHeading}>Пленочная I & 0</h2>
-					<div 
-						tabIndex={0} 
-						onClick={() => setMobileMenuExpanded(!isMobileMenuExpanded)} 
-						className={styles.NavMobile}
-					>
-							<span></span>
-							<span></span>
-							<span></span>
-					</div>
-					{isMobileMenuExpanded && (
-						<ul className={styles.NavMobileExpanded}>
-							{MobileRoutes.map(route => (
-								<Fragment key={route.caption}>
-									<li role="menuitem" className={cx(styles.LinkBlock, {
-										[styles.NavMobileActive]: route.link === currentRoute
+	return (
+		<ul className={styles.DesktopNav}>
+			{Routes.map(route => (
+				<li key={route.caption} role="menuitem" className={cx(styles.LinkBlock, {
+					[styles.Active]: route.link === currentRoute
+				})}>
+					<Link href={route.link || '/'}>
+						<a className={styles.Link} aria-haspopup={!!route.subRoutes}>
+							{route.caption}
+						</a>
+					</Link>
+					{route.subRoutes && (
+						<menu className={styles.SubMenu}>
+							<ul className={styles.SubMenuList}>
+								{route.subRoutes.map((subRoute) => (
+									<li key={subRoute.link} className={cx(styles.LinkBlock, {
+										[styles.Active]: subRoute.link === currentRoute
 									})}>
-										<Link href={route.link || '/'}>
-											<a className={styles.Link}>{route.caption}</a>
+										<Link href={subRoute.link}>
+											<a className={styles.SubMenuLink}>{subRoute.caption}</a>
 										</Link>
 									</li>
-								</Fragment>
-							))}
-						</ul>
+								))}
+							</ul>
+						</menu>
 					)}
-				</Fragment>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+function MobileNav() {
+	const [isMobileMenuExpanded, setMobileMenuExpanded] = useState(false);
+	const { asPath: currentRoute } = useRouter();
+
+	return (
+		<div className={styles.NavMobileBlock}>
+			<h2 className={styles.NavMobileHeading}>Пленочная I & 0</h2>
+			<div 
+				tabIndex={0} 
+				onClick={() => setMobileMenuExpanded(!isMobileMenuExpanded)} 
+				className={styles.NavMobile}
+			>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			{isMobileMenuExpanded && (
+				<ul className={styles.NavMobileExpanded}>
+					{MobileRoutes.map(route => (
+						<li key={route.caption} role="menuitem" className={cx({
+							[styles.NavMobileActive]: route.link === currentRoute
+						})}>
+							<Link href={route.link || '/'}>
+								<a className={styles.Link}>{route.caption}</a>
+							</Link>
+						</li>
+					))}
+				</ul>
 			)}
+		</div>
+	)
+}
+
+export default function Navbar() {
+  return (
+    <nav aria-label="Основная навигация" className={styles.Nav}>
+      <MobileNav />
+			<DesktopNav />
     </nav>
   );
 };
