7ca9c70f09729b52a53b85e61c2c1d34f9d8d60f
rayetzki <rayetzkiillya@gmail.com>
2021-09-26 11:54:13 +0300

    [Client]: Dark theme support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {	
+	webpack(config) {
+		config.module.rules.push({
+      test: /\.svg$/,
+     	issuer: { and: [/\.(js|ts)x?$/] },
+      use: ['@svgr/webpack'],
+    });
+
+		return config;
+	},
   reactStrictMode: true,
 	images: {
     domains: ['res.cloudinary.com'],
